2fae4522b5d21ccb4a5c8f05d8f448b747fe8858
Andrew Chung <archung@gmail.com>
2014-06-01 23:24:40 +0100

    Okay, this is the better version

--- Assets/Scripts/NetworkedPlayer.cs
+++ Assets/Scripts/NetworkedPlayer.cs
@@ -5,12 +5,17 @@ public class NetworkedPlayer : MonoBehaviour
 {
 
   public float lerpFactor = 10f;
-  private Vector3 lastReceievedPosition;
 
-  void Start()
-  {
-    lastReceievedPosition = transform.position;
-  }
+  public float interpolationBackTime = 0.1f;
+
+  // private Vector3 lastReceievedPosition;
+  private NetworkState[] stateBuffer = new NetworkState[20];
+  private int stateCount;
+
+//  void Start()
+//  {
+//    lastReceievedPosition = transform.position;
+//  }
 
   void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
   {
@@ -25,15 +30,67 @@ public class NetworkedPlayer : MonoBehaviour
       stream.Serialize(ref position);
       // Debug.Log("Okay, reading network object: " + position);
       // transform.position = position;
-      lastReceievedPosition = position;
+      //lastReceievedPosition = position;
+      SaveBuffer(new NetworkState(position, info.timestamp));
     }
   }
 
   void Update()
   {
-    if (!networkView.isMine)
+    if (networkView.isMine || stateCount == 0)
     {
-      transform.position = Vector3.Lerp(transform.position, lastReceievedPosition, Time.deltaTime * lerpFactor);
+      return;
+    }
+
+    double currentTime = Network.time;
+    double interpolationTime = currentTime - interpolationBackTime;
+
+    if (stateBuffer[0].timestamp > interpolationTime)
+    {
+      for (int i = 0; i < stateCount; i++)
+      {
+        if (stateBuffer[i].timestamp <= interpolationTime || i == stateCount - 1)
+        {
+          NetworkState lhs = stateBuffer[i];
+
+          NetworkState rhs = stateBuffer[Mathf.Max(i - 1, 0)];
+
+          double length = rhs.timestamp - lhs.timestamp;
+          float t = 0f;
+          if (length > 0.0001)
+          {
+            t = (float)((interpolationTime - lhs.timestamp) / length);
+          }
+
+          transform.position = Vector3.Lerp(lhs.position, rhs.position, t);
+          break;
+        }
+      }
+    }
+  }
+
+  void SaveBuffer(NetworkState state)
+  {
+    for (int i = stateBuffer.Length - 1; i > 0; i--)
+    {
+      stateBuffer[i] = stateBuffer[i - 1];
+    }
+
+    // state[0] is the most current??
+    stateBuffer[0] = state;
+
+    stateCount = Mathf.Min(stateCount + 1, stateBuffer.Length);
+  }
+
+  private struct NetworkState
+  {
+    public Vector3 position;
+    public double timestamp;
+
+    public NetworkState(Vector3 pos, double time)
+    {
+      this.position = pos;
+      this.timestamp = time;
     }
   }
 }
