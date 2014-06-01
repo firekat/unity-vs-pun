db594ed79df0d76b94be916e126cf42f6de8ec11
Andrew Chung <archung@gmail.com>
2014-06-01 21:48:32 +0100

    Okay, 2nd commit, this is where i pretty much got with chung pong

--- Assets/Scripts/NetworkedPlayer.cs
+++ Assets/Scripts/NetworkedPlayer.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NetworkedPlayer : MonoBehaviour {
+public class NetworkedPlayer : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+  public float lerpFactor = 10f;
+  private Vector3 lastReceievedPosition;
+
+  void Start()
+  {
+    lastReceievedPosition = transform.position;
+  }
+
+  void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+  {
+    Vector3 position = Vector3.zero;
+    if (stream.isWriting)
+    {
+      position = transform.position;
+      stream.Serialize(ref position);
+    }
+    else if (stream.isReading)
+    {
+      stream.Serialize(ref position);
+      // Debug.Log("Okay, reading network object: " + position);
+      // transform.position = position;
+      lastReceievedPosition = position;
+    }
+  }
+
+  void Update()
+  {
+    if (!networkView.isMine)
+    {
+      transform.position = Vector3.Lerp(transform.position, lastReceievedPosition, Time.deltaTime * lerpFactor);
+    }
+  }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,18 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
   public float MoveSpeed = 5f;
+  void Start()
+  {
+    if (networkView == null || networkView.isMine)
+    {
+      gameObject.name = "Player - Me";
+    }
+    else
+    {
+      gameObject.name = "Player " + networkView.owner;
+    }
+  }
+
   // Update is called once per frame
   void Update()
   {
